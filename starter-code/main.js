8eb9861854f7d5fc7bdd61eb305e1656637f7671
Ryan Thomas <Ryan Thomas>
2016-11-27 19:01:03 -0500

    created alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,13 @@ var cardFour = "king";
 
 cardOne == cardTwo;
 cardThree == cardFour;
+
+if(cardOne === cardTwo) {
+	alert("You found a match!");
+}
+else if(cardOne === cardThree) {
+	alert("Sorry, try again.");
+}
+else if(cardOne === cardFour) {
+	alert("Sorry, try again.");
+}
